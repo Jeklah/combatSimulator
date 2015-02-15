8b7178c1f6dc2aa48aff45f9d71108afb4eb039c
Jeklah <arthurbowers@gmail.com>
2015-02-15 01:14:05 +0000

    Adding content to inventory.h

--- inventory.h
+++ inventory.h
@@ -1,7 +1,26 @@
 #pragma once
 
 #include "stdafx.h"
-#include "ItemTable.h"
+#include "itemTable.h"
 
 #include <iostream>
 #include <vector>
+
+class inventory{
+	private:
+		int gold:
+		int size;
+		int usedSize;
+		int freeSize;
+		std::vector<int> invIndex;
+		itemTable* itemTable;
+	
+	public:
+		inventory(itemTable* itemTable_, int startingSize_);
+		int getGold();
+		int getSize();
+		int getUsedSize();
+		int getFreeSize();	
+		void addItem(int itemID);
+		void removeItem(int itemID);
+};
