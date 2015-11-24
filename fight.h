21ffc32d227791fabd7ebc3331a47b5ebca516da
Jeklah <arthurbowers@gmail.com>
2015-11-24 17:42:42 +0000

    Adding fight class.

---
+++ fight.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include "stdafx.h"
+#include <iostream>
+#include "enemy.h"
+#include "player.h"
+
+#ifndef FIGHT_H
+#define FIGHT_H
+
+using namespace std;
+
+class fight
+{
+
+	public:
+		fight(player*, enemy*);
+
+};
+
+#endif
